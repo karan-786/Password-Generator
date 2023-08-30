b794f01ac27c060175573804a0601d4652cd6e1d
Karan Sahare <92965943+karan-786@users.noreply.github.com>
2023-08-30 21:07:40 +0530

    Add files via upload

---
+++ Js.js
@@ -0,0 +1,8 @@
+const passwordBox = document.getElementById("passsword");
+const length  = 12;
+
+
+const uppercase= "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+console.log(uppercase.toLowerCase)
+console.log("hello")
+alert(":bweifb")
